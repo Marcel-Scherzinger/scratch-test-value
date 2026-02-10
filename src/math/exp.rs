f53d68781075f48afedf7c0a40a4566269d5ce9c
Marcel Scherzinger <>
2026-02-10 12:52:54 +0100

    feat(math): add natural and decadic logarithm (#13, #14)

--- src/math/exp.rs
+++ src/math/exp.rs
@@ -7,9 +7,19 @@ impl SNumber {
     {
         SNumber::Float(self.q_as_float_nan_is_zero(sink).exp())
     }
-    #[allow(unused)]
-    pub(crate) fn exp(&self) -> SNumber {
-        self.q_exp(&mut ())
+
+    pub fn q_ln<Q>(&self, sink: &mut Q) -> SNumber
+    where
+        Q: QuirkSink<SNumberToFloatQ>,
+    {
+        Self::Float(self.q_as_float_nan_is_zero(sink).ln())
+    }
+
+    pub fn q_log10<Q>(&self, sink: &mut Q) -> SNumber
+    where
+        Q: QuirkSink<SNumberToFloatQ>,
+    {
+        Self::Float(self.q_as_float_nan_is_zero(sink).log10())
     }
 
     /// This computes `10^value` where value is the number in `self`
@@ -34,8 +44,20 @@ impl SNumber {
     }
 
     #[allow(unused)]
+    pub(crate) fn exp(&self) -> SNumber {
+        self.q_exp(&mut ())
+    }
+    #[allow(unused)]
+    pub(crate) fn ln(&self) -> SNumber {
+        self.q_ln(&mut ())
+    }
+    #[allow(unused)]
     pub(crate) fn power_of_10(&self) -> SNumber {
         self.q_power_of_10(&mut ())
+    }
+    #[allow(unused)]
+    pub(crate) fn log10(&self) -> SNumber {
+        self.q_log10(&mut ())
     }
 }
 
@@ -44,25 +66,40 @@ mod tests {
     use crate::SNumber;
 
     #[test]
-    fn test_exp_0() {
-        assert_eq!(SNumber::Int(1), SNumber::Float(0.0).exp());
+    fn test_ln_special() {
+        assert_eq!(SNumber::NEG_INFINITY, SNumber::Float(0.0).ln());
+        assert_eq!(SNumber::POS_ZERO, SNumber::Float(1.0).ln());
+        assert_eq!(SNumber::NEG_ZERO, SNumber::Float(1.0).ln());
+        assert_eq!(SNumber::INFINITY, SNumber::INFINITY.ln());
+        assert_eq!(SNumber::NAN, SNumber::NEG_INFINITY.ln());
+        assert_eq!(SNumber::NEG_INFINITY, SNumber::NAN.ln());
     }
+
     #[test]
-    fn test_10_to_0() {
-        assert_eq!(SNumber::Int(1), SNumber::Float(0.0).power_of_10());
+    fn test_log10_special() {
+        assert_eq!(SNumber::NEG_INFINITY, SNumber::Float(0.0).log10());
+        assert_eq!(SNumber::POS_ZERO, SNumber::Float(1.0).log10());
+        assert_eq!(SNumber::NEG_ZERO, SNumber::Float(1.0).log10());
+        assert_eq!(SNumber::INFINITY, SNumber::INFINITY.log10());
+        assert_eq!(SNumber::NAN, SNumber::NEG_INFINITY.log10());
+        assert_eq!(SNumber::NEG_INFINITY, SNumber::NAN.log10());
     }
 
     #[test]
     fn test_exp_special() {
+        assert_eq!(SNumber::Int(1), SNumber::Float(0.0).exp());
         assert_eq!(SNumber::INFINITY, SNumber::INFINITY.exp());
         assert_eq!(SNumber::POS_ZERO, SNumber::NEG_INFINITY.exp());
         assert_eq!(SNumber::NEG_ZERO, SNumber::NEG_INFINITY.exp());
         assert_eq!(SNumber::Float(1.0), SNumber::NAN.exp());
         assert_eq!(SNumber::Int(1), SNumber::NAN.exp());
+
+        assert_eq!(SNumber::INFINITY, SNumber::Float(10000.0).exp());
     }
 
     #[test]
     fn test_10_to_special() {
+        assert_eq!(SNumber::Int(1), SNumber::Float(0.0).power_of_10());
         assert_eq!(SNumber::INFINITY, SNumber::INFINITY.power_of_10());
         assert_eq!(SNumber::POS_ZERO, SNumber::NEG_INFINITY.power_of_10());
         assert_eq!(SNumber::NEG_ZERO, SNumber::NEG_INFINITY.power_of_10());
