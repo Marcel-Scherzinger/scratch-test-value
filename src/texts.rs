527cf1b557c2b34b1ec4985fabd15dbb8ca040df
Marcel Scherzinger <>
2026-02-10 16:11:10 +0100

    feat(texts): simple string length limitation

--- src/texts.rs
+++ src/texts.rs
@@ -5,11 +5,21 @@ pub use nth_letter::NthLetterOfTextQ;
 use crate::{ARc, SValue};
 
 impl SValue {
+    pub fn concat_no_limit(&self, other: &SValue) -> ARc<str> {
+        self.concat(other, usize::MAX).unwrap_or_default()
+    }
+
     /// Concatenates this value (as string) with another value (as string)
-    pub fn concat(&self, other: &SValue) -> ARc<str> {
-        let mut s = self.as_text().to_string();
-        s += other.as_text().as_ref();
-        s.into()
+    pub fn concat(&self, other: &SValue, max_string_length: usize) -> Option<ARc<str>> {
+        let my_text = self.as_text();
+        let ot_text = other.as_text();
+        if my_text.len() + ot_text.len() > max_string_length {
+            return None;
+        }
+
+        let mut s = my_text.to_string();
+        s += ot_text.as_ref();
+        Some(s.into())
     }
 
     /// Does this text contain another text?
